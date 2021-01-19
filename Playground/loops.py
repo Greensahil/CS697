74820420e0fa576448d00b2ae427c7cead9d80e1
unknown <ssharma2@svsu.edu>
2021-01-19 18:08:15 -0500

    add digits of a number

--- Playground/loops.py
+++ Playground/loops.py
@@ -1,4 +1,17 @@
-count = 0
-while count < 5:
-    print("While loop", count)
-    count = count + 1
+
+
+
+num = input("Enter number")
+num = int(num)
+
+
+sum = 0
+
+while(num > 0):
+    remainder = num % 10
+    sum = sum + remainder
+    num = num // 10
+
+print(sum)
+
+
