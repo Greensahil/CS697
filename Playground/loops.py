b682644e7b33ec6ce1ac6c3ac1e431fc4317773f
unknown <ssharma2@svsu.edu>
2021-01-19 17:53:08 -0500

    while loop

---
+++ Playground/loops.py
@@ -0,0 +1,4 @@
+count = 0
+while count < 5:
+    print("While loop", count)
+    count = count + 1
---
+++ Playground/week2.py
@@ -0,0 +1,16 @@
+num = input("Enter a number")
+
+num = int(num)
+
+if (num % 5 == 0) and  (num % 7 == 0):
+    print("Go SVSU")
+elif num % 5 == 0 :
+    print("Go")
+elif num % 7 == 0:
+    print("SVSU")
+   
+
+
+
+
+
