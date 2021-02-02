744353f0c4f60245445fbe693c0cf7ac9c5bf11f
unknown <ssharma2@svsu.edu>
2021-02-02 18:12:51 -0500

    python documentation

---
+++ Playground/listAndTuples.py
@@ -0,0 +1,74 @@
+#sequence an object that contains multiple items of data
+#list is mutable can be changed in place in memory 
+#tuple cannot be modified unless you are reassigining to a different place in memory
+
+list = [1,2]
+
+print(list)
+
+#list is similar to array list in java
+#list is dynamic and we can change the size
+
+#list can be herttogenous
+list = [1,2, 'some string', 1.34343]
+print(list)
+
+for item in range(len(list)):
+    print(list[item])
+
+print(len(list))
+
+
+list2 = ['asdas','asdas']
+print(list + list2)
+print(list.extend(list2)) #same thing
+
+l1 = [1,2]
+print(l1*5) #[1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+
+
+for item in list:
+    print(item)
+
+
+print(list[-1]) #last element in the list
+
+
+#since list are mutable 
+
+list[1] = 5
+
+#list slicing
+
+#start index is inclusive and lst idex is not. Last one is step if -ve it starts from the right
+l1[1:] #print everything from start to end of the list
+
+print(l1[0:5:2])
+
+#check to see if a list contains something
+print(0 in l1)
+
+#add to the end of the list
+l1.append(9)
+
+
+
+
+print(l1.index(9))
+
+#insert at any point in the list
+l1.insert(1,4)
+l1.sort()
+
+#Remove by value and not index
+l1.remove(4)
+
+#By index and it also returns the value that was removeed
+print(li.pop(1))
+
+print(l1)
+
+
+#list are mutable so to copy youcannot do
+#l2 = l1
+#you can create a loop to copy each element
---
+++ Playground/scope.py
@@ -0,0 +1,11 @@
+#Scope of variables
+
+#Local sope: Always searches for local scope
+#Global scope: 
+
+a = 5
+
+def foo():
+    print (a)
+
+foo()
